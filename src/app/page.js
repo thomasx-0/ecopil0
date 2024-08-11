8dcd7acc7bc85d4f0b94ae1072020d1a4ba7c0e7
v9zpfr5qff <pennant.t@aol.com>
2024-08-10 23:24:59 -0400

    Added coming soon buttons

--- src/app/page.js
+++ src/app/page.js
@@ -31,24 +31,26 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Sign up/Sign in{" "}
+            Sign up/Sign in{" "} <span
+              className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">coming soon</span>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-           Sign in to your student profile.
+            Sign in to your student profile.
           </p>
         </a>
 
         <a
-          href="#"
+            href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Sample Worksheets{" "}
+            Sample Worksheets{" "} <span
+              className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">coming soon</span>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -65,13 +67,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            AI Solution Analyzer (coming soon){" "}
+            AI Solution Analyzer{" "} <span
+              className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">coming soon</span>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-           Upload LATEX solutions for feedback with subscription.
+            Upload LATEX solutions for feedback with subscription.
           </p>
         </a>
 
@@ -82,13 +85,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Feedback/Contact{" "}
+            Feedback/Contact{" "} <span
+              className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">coming soon</span>
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-           Send a message to the tutoring team.
+            Send a message to the tutoring team.
           </p>
         </a>
       </div>
