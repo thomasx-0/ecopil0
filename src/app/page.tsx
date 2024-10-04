9f581caab1299f5c439b8d503ff2942e77d94b95
Thomas Pennant <127155153+thomasx-0@users.noreply.github.com>
2024-10-04 14:16:44 -0400

    Add new Bento Grid component to home page
    Introduced a new 'Grid' component in `bento.tsx` to display deploy features. Integrated the new component into the homepage layout for better content visualization. This enhances the UI with sections for mobile friendliness, performance, and security.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import {Footer} from "@/app/components/footer";
 import { Navbar } from "@/app/components/nav";
 import { TwitterEmbed } from "@/app/components/xpost";
+import { Grid } from "@/app/components/bento";
 
 
 export default function Home() {
@@ -10,8 +11,9 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col justify-around p-20 gap-7 font-sans text-base">
             <Navbar />
-            <div className="flex flex-row justify-around p-20 gap-7 font-sans">
+            <div className="flex flex-row justify-around p-20 gap-7 font-sans h-max">
                <TwitterEmbed />
+                <Grid />
             </div>
            <Footer />
         </main>
