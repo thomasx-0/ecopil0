87809102efe373b75eb188e2edb7fb71ac465cdc
Thomas Pennant <127155153+thomasx-0@users.noreply.github.com>
2024-10-19 09:34:43 -0400

    Remove TwitterEmbed and integrate Content component
    Replaced the TwitterEmbed component with a new Content component that provides detailed information about reducing personal carbon footprints. Updated dependencies to include Heroicons and ensure compatibility.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 "use client"
 import React from "react"
 import {Footer} from "./components/footer";
-import { Navbar } from "./components/nav";
-import { TwitterEmbed } from "./components/xpost";
+import {Navbar} from "./components/nav";
+import {Content} from "./components/content";
+import {Fadein} from "./components/fadein";
 
 export default function Home() {
 
     return (
         <main className="flex flex-col items-center justify-center min-h-screen py-8 px-2 sm:px-4 lg:px-6p-20 gap-7 text-base font-[family-name:var(--font-geist-sans)]">
             <Navbar/>
-            <div className={"flex flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center p-10 gap-7 text-4xl"}>
-                <h1> Reduce and Reuse </h1>
-            </div>
-                <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center gap-7">
-                    <TwitterEmbed/>
-                </div>
+            <Fadein>
+               <Content />
+            </Fadein>
             <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center gap-7">
                <Footer/>
             </div>
