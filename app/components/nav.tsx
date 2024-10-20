6021fa5c339801289910426309719e704692f741
Thomas Pennant <127155153+thomasx-0@users.noreply.github.com>
2024-10-19 22:30:38 -0400

    Change 'About' link to 'Blog'
    Updated the navigation links in both mobile and desktop components to replace 'About' with 'Blog'. This ensures consistency across the site navigation for a better user experience.

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -85,7 +85,7 @@ function MobileNavigation(
         </div>
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-            <MobileNavItem href="/about">About</MobileNavItem>
+            <MobileNavItem href="/blog">Blog</MobileNavItem>
             <MobileNavItem href="/store">e-Store</MobileNavItem>
             <MobileNavItem href="/uses">Uses</MobileNavItem>
               <Downloader />
@@ -130,7 +130,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
     <nav {...props}>
         <div className="flex flex-row items-center justify-between">
             <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                <NavItem href="/about">About</NavItem>
+                <NavItem href="/blog">Blog</NavItem>
                 <NavItem href="/store">e-Store</NavItem>
                 <NavItem href="/uses">Uses</NavItem>
             </ul>
